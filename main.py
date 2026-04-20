1b3650eeffb4d02af2906da73125c7b9ce604ac3
user762704 <aungchitmyo.npw@gmail.com>
2026-04-20 12:05:12 +0630

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-??
+import os
+import asyncio
+from telethon import TelegramClient
+from telethon.tl.functions.messages import SendReactionRequest
+
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+CHANNEL = os.getenv("CHANNEL")
+POST_ID = int(os.getenv("POST_ID"))
+
+SESSION = "session"
+
+async def main():
+    client = TelegramClient(SESSION, API_ID, API_HASH)
+    await client.start()
+
+    await client(SendReactionRequest(
+        peer=CHANNEL,
+        msg_id=POST_ID,
+        reaction=['❤️']
+    ))
+
+    print("Reacted ❤️")
+    await client.disconnect()
+
+asyncio.run(main())
